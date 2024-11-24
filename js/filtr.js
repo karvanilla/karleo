c20a3ce86af5962171e6a8ffe98736ad114bd730
karvanilla <158720362+karvanilla@users.noreply.github.com>
2024-11-24 15:02:49 +0300

    Update filtr.js

--- js/filtr.js
+++ js/filtr.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function displayNotification(message) {
-    const existingNotification = document.querySelector(".notification");
+    const existingNotification = document.querySelector(".filtr");
     if (existingNotification) existingNotification.remove();
 
     const notification = document.createElement("div");
